7136cba2eff3450e5ed9dfce4619668df3e6e174
Vaibhav Guatam <gautamvaibhav56@gmail.com>
2025-08-02 01:50:19 +0530

    Update Dropdown.tsx
    --

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -27,7 +27,7 @@ const Dropdown: React.FC = () => {
     <select
       value={selected}
       onChange={handleChange}
-      className="rounded animate-bounce cursor-pointer hover:animate-none px-2 py-1 sm:mx-6 hover:opacity-60 transition-all duration-500"
+      className="rounded animate-bounce cursor-pointer hover:animate-none px-2 py-1 sm:mr-10 hover:opacity-60 transition-all duration-500"
     >
       {themeConfig.map((ele: ThemeConfigItem) => (
         <option className="text-black" key={ele.id} value={ele.name}>
